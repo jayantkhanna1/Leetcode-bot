0618369ebb3dde84ec49c64a14d66d6e400bc454
jayantkhanna1 <jayantkhanna3105@gmail.com>
2022-11-04 08:33:32 +0530

    Aah yes as soon as code start working change your UI leetcode. Humor based on my pain

--- task.py
+++ task.py
@@ -1,6 +1,8 @@
 from RPA.Browser.Selenium import Selenium
 import time 
 import json
+import requests
+from bs4 import BeautifulSoup
 
 
 browser = Selenium()
@@ -50,20 +52,40 @@ class Login:
         if login_method == "github":
                 logged_in = LoginMethods.login_via_github(data)
                 return logged_in
-           
+        elif login_method == "leetcode":
+                print("Leetcode Login not implemented")
+                return False
+        elif login_method == "google":
+                print("Google Login not supported yet")
+                return False
+        else:
+                print("Invalid Login Method")
+                return False
 
+class Answer:
+    def get_answer(answer_link):
+        # Getting answer
+        answer = requests.get(answer_link)
+        soup = BeautifulSoup(answer.content, 'html.parser')
+        return soup
 
 class Main:
     def launch_the_rocket():
+        # CHECKS IF LOGGED IN
         logged_in = Login.login()
         if logged_in:
+            # Gets Json data
             data = Details.get_details()
             if data == "0":
+                # In case of error
                 return
+            # Use this if we have to do question by link
             if data["do_questions_by_links"] == True:
-                
+                # Gets list of questions
                 list_of_questions = data["list_of_questions"]
+                # Iterates over list of questions
                 for x in list_of_questions:
+                    # Getting answer links by matching question name
                     question_name = x.replace('https://leetcode.com/problems/','')
                     question_name = question_name.replace('-',' ')
                     if question_name[len(question_name)-1] == "/":
@@ -72,42 +94,83 @@ class Main:
                     f = open("answers.json", "r")
                     answers = json.load(f)
                     for z in answers:
+                        # If question name matches with the answer name
                         if z["question_title"].lower() == question_name.lower():
-                            browser.go_to(z["answer_link"])
-                            # copy_commands = "CTRL+a CTRL+c"
-                            # browser.press_keys("xpath:/html/body/pre", copy_commands)
-                            time.sleep(10)
-                            code = browser.get_text("tag:pre")
-                            
-                            # browser.press_keys("tag:pre","\ue009+a \ue009+c")
+                            # Gets answer link
+                            code = Answer.get_answer(z["answer_link"])
+
+                            # Gets code and goes to question
                             browser.go_to(x)
-                            time.sleep(10)
-                            # select python 3
-                            #browser.click_element("class:ant-select-selection-selected-value")
-                            # not selecting for some reason check this
+                            time.sleep(data["wait_time_for_page_load"])
 
-                            #browser.click_element("xpath:/html/body/div[6]/div/div/div/ul/li[1]")
-                            #time.sleep(5)
-                            # select editor
-
-                            # paste new code in correct formatting
-                            # subm
-                            # select_commands = "SHIFT+RIGHT"
-                            # for x in range(0,1000):
-                            #     browser.press_keys("class:CodeMirror-sizer", select_commands)
+                            # Removes already present code
                             select_commands = "CTRL+a Backspace"
                             browser.press_keys("class:CodeMirror-sizer", select_commands)
+
+                            # Copies code to clipboard
                             import pyperclip
                             pyperclip.copy(code)
+
+                            # Pastes code in editor and sleeps
                             paste_commands = "CTRL+v"
                             browser.press_keys("class:CodeMirror-sizer", paste_commands)
-                            # Code error
-                            # for x in code:
-                            #     browser.press_keys("class:CodeMirror-sizer", x)
-                            time.sleep(20)
 
+                            # Sleeping before submitting
+                            time.sleep(data["wait_time_for_submitting_answer"])
+
+                            # Submits answer
                             browser.click_element("class:submit__2ISl")
-                            time.sleep(10)
+
+                            # Sleeping before moving on to next question
+                            time.sleep(data["wait_time_for_submitting_answer"])
+
+                            # Printing answer passed or not
+                            try:
+                                if browser.get_text("class:success__3Ai7") == "Success":
+                                    print("Question number - "+str(z["question_number"])+" Solved")
+                                else:
+                                    print("Question number - "+str(z["question_number"])+" Failed")
+                            except:
+                                print("Question number - "+str(z["question_number"])+" Failed")
+        
+            elif data["do_questions_by_links"] == False:
+                range_of_questions = data["list_of_questions_by_range"]
+                for x in range(range_of_questions[0], range_of_questions[1]+1):
+                    # Gets answer links by matching question name
+                    f = open("answers.json", "r")
+                    answers = json.load(f)
+                    for z in answers:
+                        # If question name matches with the answer name
+                        if z["question_number"] == x:
+                            # Gets answer link
+                            code = Answer.get_answer(z["answer_link"])
+
+                            # Gets code and goes to question
+                            browser.go_to("https://leetcode.com/problems/"+z["question_title"].replace(' ','-').lower())
+                            time.sleep(data["wait_time_for_page_load"])
+
+                            # Removes already present code
+                            select_commands = "CTRL+a Backspace"
+                            browser.press_keys("class:CodeMirror-sizer", select_commands)
+
+                            # Copies code to clipboard
+                            import pyperclip
+                            pyperclip.copy(code)
+
+                            # Pastes code in editor and sleeps
+                            paste_commands = "CTRL+v"
+                            browser.press_keys("class:CodeMirror-sizer", paste_commands)
+
+                            # Sleeping before submitting
+                            time.sleep(data["wait_time_for_submitting_answer"])
+
+                            # Submits answer
+                            browser.click_element("class:submit__2ISl")
+
+                            # Sleeping before moving on to next question
+                            time.sleep(data["wait_time_for_submitting_answer"])
+
+                            # Printing answer passed or not
                             try:
                                 if browser.get_text("class:success__3Ai7") == "Success":
                                     print("Question number - "+str(z["question_number"])+" Solved")
